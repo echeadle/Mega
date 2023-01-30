358add559cd4ab42a48f554abe74490ef1aab139
echeadle <echeadle@gmail.com>
2023-01-30 15:13:41 -0700

    Finish bug fixing exercise

---
+++ app1/BugFixing/bug_fixing_exercises_1-4.py
@@ -0,0 +1,13 @@
+# Exercise 1
+answers = ['Yes', 'No', 'Yes', 'No', 'No']
+
+# Exercise 2
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+print(answers)
+
+# Exercise 3
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+print(answers)
