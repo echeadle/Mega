c496797e20543e75d405cd80ae011a80e8101ed7
echeadle <echeadle@gmail.com>
2023-02-01 08:26:23 -0700

    Add doc strings to functions

--- 13_day/main.py
+++ 13_day/main.py
@@ -1,10 +1,14 @@
 def get_todos(filepath='files/todos.txt'):
+    """ Read a text file and return the list of
+        to-do items.
+    """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath='files/todos.txt'):
+    """ Write the to-do items list to a text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
