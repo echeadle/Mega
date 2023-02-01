549d1d0dd38c94bb2e1a59b85b160e8b3620aeef
echeadle <echeadle@gmail.com>
2023-02-01 08:01:01 -0700

    Start day 13

---
+++ 12_day/bonus.py
@@ -0,0 +1,20 @@
+feet_inches = input("Enter feet and inches: ")
+
+
+def convert(feet_inches):
+
+    parts = feet_inches.split(" ")
+    feet = float(parts[0])
+    inches = float(parts[1])
+
+    meters = feet * 0.3048 + inches * 0.0254
+    # return f"{feet} feet and {inches} inches is equal to {meters} meters."
+    return meters
+
+
+print(convert(feet_inches))
+results = convert(feet_inches)
+if results < 1:
+    print("Kid is too small.")
+else:
+    print("Kids can use this slide.")
