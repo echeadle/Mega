0ee63cc598dcf2a05167b5203623d187c13e4c53
echeadle <echeadle@gmail.com>
2023-02-05 18:36:06 -0700

    Add todo items.

--- 19_day/web.py
+++ 19_day/web.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import functions as fnc
 
+
 todos = fnc.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    fnc.write_todos(todos)
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
@@ -12,4 +19,7 @@ for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
+st.session_state
